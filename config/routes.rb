3c5008d2b021f071ed3c79c74ff6632ce335b95a
James Lee <james.lee@jivesoftware.com>
2013-04-06 15:34:56 -0700

    Adding user login/registration;

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Commuterapp::Application.routes.draw do
+  devise_for :users
+
   resources :commutetypes
 
 
   resources :commutes
 
+
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130406203944) do
+ActiveRecord::Schema.define(:version => 20130406221603) do
 
-  create_table "commutes", :force => true do |t|
-    t.string   "name"
-    t.datetime "start"
-    t.datetime "end"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+# Could not dump table "commutes" because of following StandardError
+#   Unknown type 'reference' for column 'user_id'
 
   create_table "commutetypes", :force => true do |t|
     t.string   "name"
@@ -27,5 +22,23 @@ ActiveRecord::Schema.define(:version => 20130406203944) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
