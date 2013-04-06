4e533be1ee3c19a4421ebd770a3d552e0af153f8
Patti <pj.97211@gmail.com>
2013-04-06 12:03:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commuterapp::Application.config.secret_token = 'd5e8ae3aa45f0347bfac6de4885b9d0ffc573dd9ef72c71c10aafb93980ad145d390020e53fb6c1a295e3280d55ec48fe265e94300deebbc0debcdf74e2ab046'
