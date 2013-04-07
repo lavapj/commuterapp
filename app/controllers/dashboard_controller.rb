5f9e42f479e89fe7c2d55fe7231f7a60da6b0168
James Lee <james.lee@jivesoftware.com>
2013-04-06 20:59:07 -0700

    Make dashboard consume a commute.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,44 +7,68 @@ class DashboardController < ApplicationController
       end
   end
 
-  def main
-    @commute = Commute.new
-
-    # mileage = 12            # testing
-    # commute_type = 2   # testing
+  def calculate
+    @commute = Commute.find(params[:id])
 
     @baseline = Commutetype.find(2).commute_attribute
-
-    # @baseline = CommuteAttribute.where("commutetype_id = ?", 2).first
-    # @mycommute = CommuteAttribute.where("commutetype_id = ?", commute_type).first
-    @passengers = 2
-
-    # person + passengers
-    # 2 * 10 miles
-    # commute passenger
+    @commuteAttributes = @commute.commutetype.first!.commute_attribute
 
     # calculate cash score
     #   car cash value - other cash value
-    # if @commute.cash_fr == true
-    #   @cash_score = @baseline.cash - @commute.cash
-    # else
-    #   @cash_score = (@baseline.cash * mileage) * (1 + @passengers) - (@commute.cash * mileage)
-    # end
+    if @commuteAttributes.cash_fr == true
+       @cash_score = @baseline.cash - @commuteAttributes.cash
+    else
+       @cash_score = (@baseline.cash * @commute.distance) * (1 + @commute.passenger) - (@commuteAttributes.cash * @commute.distance)
+    end
 
-    # # calculate calories score
-    # if @commute.calories_fr == true
-    #   @cal_score = @commute.calories
-    # else
-    #   @cal_score = @commute.calories * mileage
-    # end
+    # calculate calories score
+    if @commuteAttributes.calories_fr == true
+       @cal_score = @commuteAttributes.calories
+    else
+       @cal_score = @commuteAttributes.calories * @commute.distance
+    end
 
-    # # calculate envfp score
-    # if @commute.envfp_fr == true
-    #   @envfp_score = @commute.envfp
-    # else
-    #   @envfp_score = (@baseline.envfp * mileage) * (1 + @passengers) - (@commute.envfp * mileage)
-    # end
+    # calculate envfp score
+    if @commuteAttributes.envfp_fr == true
+       @envfp_score = @commuteAttributes.envfp
+    else
+       @envfp_score = (@baseline.envfp * @commute.distance) * (1 + @commute.passengers) - (@commuteAttributes.envfp * @commute.distance)
+    end
 
+    format.json { render cash_score: @cash_score, cal_score: @cal_score, envfp_score: @envfp_score }
+  end
+
+  def main
+    if params[:id] != nil
+      @commute = Commute.find(params[:id])
+
+      @baseline = Commutetype.find(2).commute_attribute
+      @commuteAttributes = @commute.commutetypes.first!.commute_attribute
+
+      # calculate cash score
+      #   car cash value - other cash value
+      if @commuteAttributes.cash_fr == true
+        @cash_score = @baseline.cash - @commuteAttributes.cash
+      else
+        @cash_score = (@baseline.cash * @commute.distance) * (1 + @commute.passenger) - (@commuteAttributes.cash * @commute.distance)
+      end
+
+      # calculate calories score
+      if @commuteAttributes.calories_fr == true
+        @cal_score = @commuteAttributes.calories
+      else
+        @cal_score = @commuteAttributes.calories * @commute.distance
+      end
+
+      # calculate envfp score
+      if @commuteAttributes.envfp_fr == true
+        @envfp_score = @commuteAttributes.envfp
+      else
+        @envfp_score = (@baseline.envfp * @commute.distance) * (1 + @commute.passenger) - (@commuteAttributes.envfp * @commute.distance)
+      end
+    else
+      @commute = Commute.new
+    end
   end
 
 end
